6f9d2749ed76efd319bf1cfd47d64e4a622d7411
RohanSangle <rohan.sangle02@gmail.com>
2023-05-16 19:19:37 +0530

    basic skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import bgDesktop from './images/bg-main-desktop.png'
+import bgMobile from './images/bg-main-mobile.png'
+import cardlogo from './images/card-logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <section>
+      <div className="container">
+        <picture>
+          <source media="(min-width: 1024px)" srcSet={bgDesktop}/>
+          <img src={bgMobile} alt=''/> 
+        </picture>
+      </div>
+
+
+      <div>
+        <div className="cards">
+          <article className='frontcard'>
+            <img src={cardlogo} alt='' />
+            <div >
+              <h2 className='cardnumber'>0000 0000 0000 0000</h2>
+              <ul className='credentials'>
+                <h3>Rohan Sangle</h3>
+                <h3>00/0000</h3>
+              </ul>
+            </div>
+          </article>
+          <article className='backcard'>
+            <p className='backcredentials'>123</p>
+          </article>
+        </div>
+
+        <div className='form'>
+          <form>
+            <div >
+              <label>CARDHOLDER NAME</label>
+              <input type="text" placeholder="Eg. Rohan Sangle "/>
+            </div>
+            <div>
+              <label>CARD NUMBER</label>
+              <input type="password" placeholder="Eg. 9591 6489 6389 1013"/>
+            </div>
+            <div >
+              <button type="submit" className="confirm_btn">Confirm</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+    </>
+  )
 }
 
-export default App;
+export default App
