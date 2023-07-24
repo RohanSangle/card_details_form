4b03c2f88f302f1b416665d739d3a6732625a211
RohanSangle <rohan.sangle02@gmail.com>
2023-07-25 00:13:48 +0530

    fixed thankyou component and added some css

--- src/App.js
+++ src/App.js
@@ -79,10 +79,12 @@ function App() {
 function ThankYou({setConfirmed}){
   return(
     <>
-    <img src={tick} alt='' className='tickblock'/>
-      <h1 className='Thank'>Thank You!</h1>
-      <p>We've added your card details</p>
-      <button onClick={()=> setConfirmed(false)} className='btn'>Continue</button>
+      <div className='thankyouform'>
+        <img src={tick} alt='' className='tickblock'/>
+        <h1 className='Thank'>Thank You!</h1>
+        <p>We've added your card details</p>
+        <button onClick={()=> setConfirmed(false)} className='btn2'>Continue</button>
+      </div>
 
     </>
 
